09f3bc4cbb8198b9835f5f3e50d065a8eb45e00c
xby020 <xby020@163.com>
2021-12-06 17:27:37 +0800

    🍖测试switch组件

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="font-bold text-3xl">
     This is index page.
-    <div>
-      <nuxt-link to="/blog" v-slot="{ navigate }">
-        <div class="w-200px h-100px bg-blue-500 rounded-lg">
-          <button @click="navigate">blog</button>
-        </div>
-      </nuxt-link>
-    </div>
+
+    <nuxt-link to="/blog" v-slot="{ navigate }">
+      <div class="w-200px h-100px bg-blue-500 rounded-lg">
+        <button @click="navigate">blog</button>
+      </div>
+    </nuxt-link>
+
+    <potato-switch></potato-switch>
+
+    <potato-custom></potato-custom>
   </div>
 </template>
 
--- windi.config.ts
+++ windi.config.ts
@@ -4,26 +4,28 @@ import { potatoTheme } from './settings';
 const { color } = potatoTheme;
 export default defineConfig({
   theme: {
-    colors: {
-      primary: {
-        light: color.light.primary,
-        DEFAULT: color.light.primary,
-        dark: color.dark ? color.dark.primary : color.light.primary
-      },
-      secondary: {
-        light: color.light.secondary,
-        DEFAULT: color.light.secondary,
-        dark: color.dark ? color.dark.secondary : color.light.secondary
-      },
-      background: {
-        light: color.light.background,
-        DEFAULT: color.light.background,
-        dark: color.dark ? color.dark.background : color.light.background
-      },
-      success: color.light.success,
-      warning: color.light.warning,
-      error: color.light.error,
-      info: color.light.info
+    extend: {
+      colors: {
+        primary: {
+          light: color.light.primary,
+          DEFAULT: color.light.primary,
+          dark: color.dark ? color.dark.primary : color.light.primary
+        },
+        secondary: {
+          light: color.light.secondary,
+          DEFAULT: color.light.secondary,
+          dark: color.dark ? color.dark.secondary : color.light.secondary
+        },
+        background: {
+          light: color.light.background,
+          DEFAULT: color.light.background,
+          dark: color.dark ? color.dark.background : color.light.background
+        },
+        success: color.light.success,
+        warning: color.light.warning,
+        error: color.light.error,
+        info: color.light.info
+      }
     }
   }
 });
