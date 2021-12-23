c5765ca5f14c09860abe2a15337d0f19993aec58
xby020 <xby020@163.com>
2021-12-23 16:05:57 +0800

    refactor(components/PotatoSlider): 阅读 hover effect 代码，尝试改装到 slider 上
    TODO:
    - 将传递 image1 和 image2 改为传递 image:string[]，参考 https://codepen.io/ashthornton/pen/KRQbMO；
    - slider切换控制；

--- components/potato/slider/PotatoSlider.vue
+++ components/potato/slider/PotatoSlider.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="w-full h-800px bg-dark-700" ref="slider"></div>
+  <div class="w-400px h-300px bg-dark-700" ref="slider"></div>
+  <button @click="animation.transitionIn">In</button>
+  <button @click="animation.transitionOUt">Out</button>
 </template>
 
 <script lang="ts" setup>
-import effect from './effect.ts';
+import { Effect } from './effect';
 const imgList = [
   'https://cdn.pixabay.com/photo/2020/12/06/16/11/ornamentation-5809248_960_720.jpg',
   'https://cdn.pixabay.com/photo/2016/11/14/09/14/cat-1822979_960_720.jpg',
@@ -11,16 +13,19 @@ const imgList = [
 ];
 
 const slider = ref(null);
+const animation = ref();
 
 onMounted(() => {
   console.log(slider.value);
-  const myAnimation = effect({
+
+  animation.value = new Effect({
     parent: slider.value,
     intensity: 0.3,
-    image1: imgList[0],
-    image2: imgList[1],
+    image1: imgList[1],
+    image2: imgList[2],
     displacementImage: '/displacementImage/triangle.png'
   });
+  console.log(animation);
 });
 </script>
 
