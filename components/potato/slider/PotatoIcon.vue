4d5ab35ef2d879cef7014bcd22915ea8c705282e
xby020 <xby020@163.com>
2022-02-23 11:04:17 +0800

    Init daisy-ui.

---
+++ components/potato/slider/PotatoIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="icon"></div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
